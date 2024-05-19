f6de5031ad97626533bac3b8e2d00ca9a22b4131
RyanDeBarros <ryan2004db@gmail.com>
2024-05-19 17:33:35 -0400

    GL triangle display.

--- Mustang/src/Main.cpp
+++ Mustang/src/Main.cpp
@@ -1,3 +1,5 @@
+#define GLEW_STATIC
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 
@@ -13,6 +15,13 @@ int main()
 	}
 	glfwMakeContextCurrent(window);
 	glfwSwapInterval(1);
+	if (glewInit() != GLEW_OK)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	std::cout << glGetString(GL_VERSION) << std::endl;
 
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -20,6 +29,20 @@ int main()
 	double time = glfwGetTime();
 	double deltaTime = 0;
 	double prevTime = time;
+
+	float data[] = {
+		-0.5f, -0.5f,
+		 0.5f, -0.5f,
+		 0.0f,  0.5f
+	};
+	unsigned int vb;
+	glGenBuffers(1, &vb);
+	glBindBuffer(GL_ARRAY_BUFFER, vb);
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), data, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	for (;;)
 	{
@@ -29,6 +52,7 @@ int main()
 		prevTime = time;
 
 		// Render here
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
