d22ce64f35252c6899144db467d069f3095d4105
RyanDeBarros <ryan2004db@gmail.com>
2024-05-19 00:34:19 -0400

    Added GLFW and GLEW, and set up initial window.

--- Mustang/src/Main.cpp
+++ Mustang/src/Main.cpp
@@ -1,7 +1,41 @@
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 int main()
 {
-	std::cout << "Welcome to Mustang Engine!" << std::endl;
-	std::cin.get();
+	if (!glfwInit())
+		return -1;
+	GLFWwindow* window = glfwCreateWindow(1440, 1080, "Mustang Engine", nullptr, nullptr);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
+
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	double time = glfwGetTime();
+	double deltaTime = 0;
+	double prevTime = time;
+
+	for (;;)
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+		time = glfwGetTime();
+		deltaTime = time - prevTime;
+		prevTime = time;
+
+		// Render here
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+		if (glfwWindowShouldClose(window))
+			break;
+	}
+
+	glfwTerminate();
+	return 0;
 }
